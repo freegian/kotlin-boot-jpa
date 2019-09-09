537145d570126921a52c1a63f2aafb62e6e5a2c3
akitomo sakuma <akitomo.sakuma@freegian.co.jp>
2019-09-09 22:29:29 +0900

    add controller endpoint

--- src/main/kotlin/com/example/demo/ShopController.kt
+++ src/main/kotlin/com/example/demo/ShopController.kt
@@ -1,10 +1,37 @@
 package com.example.demo
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/shop")
 @RestController
 class ShopController(val service: ShopService) {
-    // add endpoint
+    @GetMapping("/{shopId}/get")
+    fun getShop(@PathVariable shopId: Long): Shop? {
+        return service.get(shopId = shopId)
+    }
+
+    @GetMapping("/getByJpql")
+    fun getByJpql(@RequestBody shopName: String): Shop? {
+        return service.getByJpql(shopName = shopName)
+    }
+
+    @GetMapping("/getBySql")
+    fun getBySql(@RequestBody shopName: String): Shop? {
+        return service.getBySql(shopName = shopName)
+    }
+
+    @PostMapping("/register")
+    fun register(@RequestBody shopName: String): Shop? {
+        return service.register(shopName = shopName)
+    }
+
+    @PostMapping("/{shopId}/update")
+    fun update(@PathVariable shopId: Long, @RequestBody shopName: String): Shop? {
+        return service.update(shopId = shopId, shopName = shopName)
+    }
+
+    @PostMapping("/{shopId}/delete")
+    fun delete(@PathVariable shopId: Long): Shop? {
+        return service.delete(shopId = shopId)
+    }
 }
